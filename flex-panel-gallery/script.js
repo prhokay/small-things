3b9d99337c06dabb69fc9759c672da4cbea947d1
Liasov Anton <pro100happy@gmail.com>
2021-04-14 08:38:34 +0300

    panel gallery

---
+++ flex-panel-gallery/script.js
@@ -0,0 +1,9 @@
+const panels = document.querySelectorAll('.panel');
+function toggleOpen() {
+	this.classList.toggle('open');
+}
+function toggleActive(e) {
+	e.propertyName.includes('flex') ? this.classList.toggle('open-active') : '';
+}
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
